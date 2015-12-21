679d6c9756f234bddc9635594015d6b7236fa717
Hari Gollapalli <hgollapalli@snaproute.com>
2015-12-21 11:27:10 -0800

    Minor changes in documentation page and db location

--- configmgr.go
+++ configmgr.go
@@ -5,11 +5,14 @@ import (
 	"github.com/gorilla/mux"
 	"models"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
 type ConfigMgr struct {
 	clients        map[string]ClientIf
+	basePath       string
+	fullPath       string
 	pRestRtr       *mux.Router
 	dbHdl          *sql.DB
 	restRoutes     []ApiRoute
@@ -65,7 +68,7 @@ func (mgr *ConfigMgr) InitializeRestRoutes() bool {
 func (mgr *ConfigMgr) InstantiateRestRtr() *mux.Router {
 	mgr.pRestRtr = mux.NewRouter().StrictSlash(true)
 	mgr.pRestRtr.PathPrefix("/api-docs/").Handler(http.StripPrefix("/api-docs/",
-		http.FileServer(http.Dir("./docsui"))))
+		http.FileServer(http.Dir(mgr.fullPath+"/docsui"))))
 
 	for _, route := range mgr.restRoutes {
 		var handler http.Handler
@@ -86,8 +89,16 @@ func (mgr *ConfigMgr) GetRestRtr() *mux.Router {
 func NewConfigMgr(paramsDir string) *ConfigMgr {
 	var rc bool
 	mgr := new(ConfigMgr)
+	var err error
+	if mgr.fullPath, err = filepath.Abs(paramsDir); err != nil {
+		logger.Printf("ERROR: Unable to get absolute path for %s, error [%s]\n", paramsDir, err)
+		return nil
+	}
+	mgr.basePath, _ = filepath.Split(mgr.fullPath)
+
 	objectConfigFile := paramsDir + "/objectconfig.json"
 	paramsFile := paramsDir + "/clients.json"
+
 	rc = mgr.InitializeClientHandles(paramsFile)
 	if rc == false {
 		logger.Println("ERROR: Error in Initializing Client handles")
@@ -101,7 +112,7 @@ func NewConfigMgr(paramsDir string) *ConfigMgr {
 	mgr.reconncetTimer = time.NewTicker(time.Millisecond * 1000)
 	mgr.InitializeRestRoutes()
 	mgr.InstantiateRestRtr()
-	mgr.InstantiateDbIf(paramsDir)
+	mgr.InstantiateDbIf()
 	logger.Println("Initialization Done!")
 	return mgr
 }
