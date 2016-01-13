e304889fc32aaf668f36cc2afe865e1d34b8cf76
Avoy Nanda <ananda@snaproute.com>
2016-01-12 16:42:22 -0800

    fix

--- apihandlers.go
+++ apihandlers.go
@@ -213,6 +213,7 @@ func ConfigObjectDelete(w http.ResponseWriter, r *http.Request) {
 	var resp ConfigResponse
 	var errCode int
 	var objKey string
+	logger.Println("ObjectDelete enter")
 	if CheckIfSystemIsReady(w) != true {
 		http.Error(w, SRErrString(SRSystemNotReady), http.StatusServiceUnavailable)
 		return
