f659b5d2da69139c71e92d31d60c0d3eaae0de2e
mjoshiSnaproute <mjoshi@Snaproute.com>
2016-06-16 14:25:47 -0700

    modified:   actions/actionMgr.go 	modified:   apis/restroutes.go 	modified:   server/configmgr.go

--- actions/actionMgr.go
+++ actions/actionMgr.go
@@ -24,6 +24,11 @@
 package actions
 
 import (
+	"config/clients"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	modelActions "models/actions"
 	"utils/logging"
 )
 
@@ -41,12 +46,66 @@ import (
 //
 
 type ActionMgr struct {
+	logger *logging.Writer
+	//	ExecuteAction(obj actions.ActionObj) error
+	ObjHdlMap map[string]ActionObjInfo
+	clientMgr *clients.ClientMgr
 }
 
 var gActionMgr *ActionMgr
 
-func InitializeActionMgr(infoFiles []string, logger *logging.Writer) *ActionMgr {
+//
+// This structure represents the json layout for action objects
+type ActionObjJson struct {
+	Owner string `json:"Owner"`
+}
+
+// This structure represents the in memory layout of all the action object handlers
+type ActionObjInfo struct {
+	Owner clients.ClientIf
+}
+
+func InitializeActionMgr(infoFiles []string, logger *logging.Writer, clientMgr *clients.ClientMgr) *ActionMgr {
 	mgr := new(ActionMgr)
+	mgr.logger = logger
+	mgr.clientMgr = clientMgr
+	if rc := mgr.InitializeActionObjectHandles(infoFiles); !rc {
+		logger.Err("Error in initializing action object handles")
+		return nil
+	}
 	gActionMgr = mgr
 	return mgr
+}
+
+func (mgr *ActionMgr) InitializeActionObjectHandles(infoFiles []string) bool {
+	var actionMap map[string]ActionObjJson
+
+	mgr.ObjHdlMap = make(map[string]ActionObjInfo)
+	for _, objFile := range infoFiles {
+		bytes, err := ioutil.ReadFile(objFile)
+		if err != nil {
+			mgr.logger.Info(fmt.Sprintln("Error in reading Action configuration file", objFile))
+			return false
+		}
+		err = json.Unmarshal(bytes, &actionMap)
+		if err != nil {
+			mgr.logger.Info(fmt.Sprintln("Error in unmarshaling data from ", objFile))
+		}
+
+		for k, v := range actionMap {
+			mgr.logger.Debug(fmt.Sprintln("For Action [", k, "] Primary owner is [", v.Owner, "] "))
+			entry := new(ActionObjInfo)
+			entry.Owner = mgr.clientMgr.Clients[v.Owner]
+			mgr.ObjHdlMap[k] = *entry
+		}
+	}
+	return true
+}
+
+func (mgr *ActionMgr) GetAllActions() []string {
+	retList := make([]string, 0)
+	for key, _ := range modelActions.ActionMap {
+		retList = append(retList, key)
+	}
+	return retList
 }
