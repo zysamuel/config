e552e74ce9360133d99fae34651f65728c50d623
Vikram Ragukumar <vragukumar@snaproute.com>
2016-06-24 16:34:11 -0700

    Add optic daemon to clients.json Enable autodiscovery of opticd objects

--- server/configmgr.go
+++ server/configmgr.go
@@ -278,6 +278,38 @@ func (mgr *ConfigMgr) DiscoverPorts() error {
 	return nil
 }
 
+func (mgr *ConfigMgr) DiscoverOpticModuleInfo() error {
+	mgr.logger.Debug("Discovering optic module information")
+	resource := "DWDMModule"
+	if objHdl, ok := modelObjs.ConfigObjectMap[resource]; ok {
+		var objs []modelObjs.ConfigObj
+		var err error
+		_, obj, _ := objects.GetConfigObj(nil, objHdl)
+		currentIndex := int64(0)
+		objCount := int64(64)
+		err, _, _, _, objs = mgr.objectMgr.ObjHdlMap[resource].Owner.GetBulkObject(obj, mgr.dbHdl.DBUtil,
+			currentIndex, objCount)
+		fmt.Println("DISCOVERING OPTICD COMPS : ", objs)
+		if err == nil {
+			for i := 0; i < len(objs); i++ {
+				dwdmCfg := (*objs[i].(*modelObjs.DWDMModule))
+				_, err := dwdmCfg.GetObjectFromDb(dwdmCfg.GetKey(), mgr.dbHdl)
+				if err != nil {
+					err = dwdmCfg.StoreObjectInDb(mgr.dbHdl)
+					if err != nil {
+						mgr.logger.Err(fmt.Sprintln("Failed to store DWDMModule config in DB ",
+							i, dwdmCfg, err))
+					} else {
+						mgr.storeUUID(dwdmCfg.GetKey())
+					}
+				}
+			}
+		}
+	}
+	mgr.logger.Debug("Optic module information discovered")
+	return nil
+}
+
 func (mgr *ConfigMgr) ConstructSystemParam(paramsDir string) []byte {
 	sysInfo := &modelObjs.SystemParam{}
 	cfgFileData, err := ioutil.ReadFile(paramsDir + "../sysprofile/systemProfile.json")
@@ -407,14 +439,17 @@ func (mgr *ConfigMgr) AutoCreateConfigObjects(paramsDir string) {
 				return
 			case "asicd":
 				mgr.DiscoverPorts()
+			case "opticd":
+				mgr.DiscoverOpticModuleInfo()
 			default:
 				mgr.logger.Info("Do Global Init for Client:" + clientName)
-				for key, value := range mgr.objectMgr.ObjHdlMap {
-					client := value.Owner
-					if value.AutoCreate && client.GetServerName() == clientName {
-						mgr.ConfigureGlobalConfig(paramsDir, key, client)
-					}
-				}
+				/*				for key, value := range mgr.objectMgr.ObjHdlMap {
+									client := value.Owner
+									if value.AutoCreate && client.GetServerName() == clientName {
+										mgr.ConfigureGlobalConfig(paramsDir, key, client)
+									}
+								}
+				*/
 			}
 			mgr.AutoDiscoverObjects(clientName)
 			mgr.ConfigureComponentLoggingLevel(clientName)
